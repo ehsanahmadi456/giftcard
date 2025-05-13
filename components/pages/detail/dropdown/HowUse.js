f883cf1e3f67c2f779e61415a32687a3737d8b29
Ehsan <ehsanahmadi8338@gmail.com>
2025-05-13 11:42:42 +0330

    change class to className

--- components/pages/detail/dropdown/HowUse.js
+++ components/pages/detail/dropdown/HowUse.js
@@ -1,6 +1,6 @@
 function HowUse() {
   return (
-    <p class="text-sm text-lowgray leading-loose h-0 hidden group-[.active]:h-max group-[.active]:flex transition-all duration-300 ease-linear">
+    <p className="text-sm text-lowgray leading-loose h-0 hidden group-[.active]:h-max group-[.active]:flex transition-all duration-300 ease-linear">
       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از
       طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان
       که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
